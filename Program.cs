572c9943166249c8bd1e2e29d8d7c19a9e5dacdf
suvarchala12 <bhumasuvarchala55@gmail.com>
2021-12-28 15:12:16 +0530

    Generics

---
+++ Program.cs
@@ -0,0 +1,13 @@
+﻿// See https://aka.ms/new-console-template for more information
+using System;
+namespace Generics
+{
+    class program
+    {
+
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Generics");
+        }
+    }
+}
